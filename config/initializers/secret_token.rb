c00dbb80880fc162a9c9fc8e8f1cf8b053c2079e
rick_barkley <rick_barkley@nuvola-networks.com>
2014-03-23 23:38:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Silverton::Application.config.secret_token = 'bc7270966bc5936dfedb63b740d54cea2076830c829a9f922dc43fa90bd5864af18f27e0ea963463d95dd4096315d79277e9e1df9233d13284cd72b63770b957'
